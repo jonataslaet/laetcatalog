4adfe27286710ba49bafd66309f92757621fcf80
jonataslaet <jonataslaetprogramador@gmail.com>
2023-06-17 09:10:38 -0300

    main: backend project created

---
+++ backend/src/main/java/com/github/jonataslaet/laetcatalog/LaetcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.github.jonataslaet.laetcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaetcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaetcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/jonataslaet/laetcatalog/LaetcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jonataslaet.laetcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaetcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
