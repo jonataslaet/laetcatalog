4765f15f19be778208518d811a623cfd0e5a89e5
jonataslaet <jonataslaetprogramador@gmail.com>
2023-08-07 10:35:45 -0300

    Refactored pagination for finding all categories

--- backend/src/main/java/com/github/jonataslaet/laetcatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/github/jonataslaet/laetcatalog/controllers/CategoryController.java
@@ -4,14 +4,12 @@ import com.github.jonataslaet.laetcatalog.controllers.dtos.CategoryDTO;
 import com.github.jonataslaet.laetcatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
@@ -21,14 +19,8 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<Page<CategoryDTO>> findAllPaged(
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "size", defaultValue = "12") Integer size,
-            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
-            @RequestParam(value = "field", defaultValue = "name") String field
-    ) {
-        PageRequest pageRequest = PageRequest.of(page, size, Sort.Direction.valueOf(direction), field);
-        Page<CategoryDTO> categories = categoryService.findAllPaged(pageRequest);
+    public ResponseEntity<Page<CategoryDTO>> findAllPaged(Pageable pageable) {
+        Page<CategoryDTO> categories = categoryService.findAllPaged(pageable);
         return ResponseEntity.ok(categories);
     }
 
