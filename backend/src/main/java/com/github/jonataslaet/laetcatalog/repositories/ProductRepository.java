a5e4cd8dd024c1846535fb6db02a02bb24a448a8
jonataslaet <jonataslaetprogramador@gmail.com>
2023-08-15 13:45:30 -0300

    Fixed sort by request param
    Put alias for every table inside sqlQuery

--- backend/src/main/java/com/github/jonataslaet/laetcatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/github/jonataslaet/laetcatalog/repositories/ProductRepository.java
@@ -14,19 +14,19 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query(nativeQuery = true, value = """
-            SELECT DISTINCT tb_product.id, tb_product.name
-            FROM tb_product
-            INNER JOIN tb_product_category ON tb_product_category.product_id = tb_product.id
-            WHERE (:categoryIds IS NULL OR tb_product_category.category_id IN :categoryIds)
-            AND (LOWER(tb_product.name) LIKE LOWER(CONCAT('%',:name,'%')))
+            SELECT DISTINCT prod.id, prod.name
+            FROM tb_product prod
+            INNER JOIN tb_product_category prodcat ON prodcat.product_id = prod.id
+            WHERE (:categoryIds IS NULL OR prodcat.category_id IN :categoryIds)
+            AND (LOWER(prod.name) LIKE LOWER(CONCAT('%',:name,'%')))
             """,
             countQuery = """
             SELECT COUNT(*) FROM (
-            SELECT DISTINCT tb_product.id, tb_product.name
-            FROM tb_product
-            INNER JOIN tb_product_category ON tb_product_category.product_id = tb_product.id
-            WHERE (:categoryIds IS NULL OR tb_product_category.category_id IN :categoryIds)
-            AND (LOWER(tb_product.name) LIKE LOWER(CONCAT('%',:name,'%')))
+            SELECT DISTINCT prod.id, prod.name
+            FROM tb_product prod
+            INNER JOIN tb_product_category prodcat ON prodcat.product_id = prod.id
+            WHERE (:categoryIds IS NULL OR prodcat.category_id IN :categoryIds)
+            AND (LOWER(prod.name) LIKE LOWER(CONCAT('%',:name,'%')))
             ) AS tb_result
             """)
     Page<ProductProjection> findAllWithTheirCategories(String name, List<Long> categoryIds, Pageable pageable);
